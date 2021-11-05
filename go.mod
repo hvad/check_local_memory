55e3ae65bee70967d53ef16a55d67874627f348c
hvad <david.hannequin@gmail.com>
2021-11-05 14:43:37 +0100

    Add initial.

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/hvad/check_local_memory
+
+go 1.16
+
+require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/shirou/gopsutil v3.21.10+incompatible
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+)
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/shirou/gopsutil/mem"
+)
+
+func main() {
+
+	warning := flag.Int("warning", 80, "Warning thresold for memory.")
+	critical := flag.Int("critical", 90, "Critical thresold for memory.")
+	flag.Parse()
+
+	m, err := mem.VirtualMemory()
+	if err != nil {
+		fmt.Printf("Can't get information - error %v.\n", err)
+		os.Exit(3)
+	}
+
+	total := m.Total / 1024 / 1024
+
+	used := m.Used / 1024 / 1024
+
+	if int(m.UsedPercent) >= *critical {
+		fmt.Printf("CRITICAL - Memory percent usage %v%% - Total Memory :%v Mo - Used Memory %v Mo\n", int(m.UsedPercent), total, used)
+		os.Exit(2)
+
+	} else if int(m.UsedPercent) >= *warning {
+		fmt.Printf("WARNING - Memory percent usage %v%% - Total Memory :%v Mo - Used Memory %v Mo\n", int(m.UsedPercent), total, used)
+		os.Exit(1)
+
+	} else {
+		fmt.Printf("OK - Memory percent usage %v%% - Total Memory :%v Mo - Used Memory %v Mo\n", int(m.UsedPercent), total, used)
+		os.Exit(0)
+	}
+
+}
